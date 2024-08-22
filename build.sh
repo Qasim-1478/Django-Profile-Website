557d8100f22ce2bf82eaa6ae093027aeeabacb4b
Qasim-1478 <115438270+Qasim-1478@users.noreply.github.com>
2024-08-22 19:36:00 +0530

    Set Up Serving Static Files

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 # Exit on error
 set -o errexit
 
+cd mywebsite
+
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+# python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
